007327114b204df1955305045fb00e73cd3ef64c
Catalina <schauflc@amazon.com>
2023-12-15 19:23:13 -0300

    ading projects 2

--- src/components/projects/allProjects.jsx
+++ src/components/projects/allProjects.jsx
@@ -52,6 +52,45 @@ const AllProjects = () => {
 							
 						</div>
 					</div>
+
+					<div className="project-container">
+
+					<div className="project-title">ESO Observatory Research - Telescope Performance Monitoring Tool Kit </div>
+					<div className="project-description">Development for monitoring analyzing and displaying relevant performance metrics for the Very Large Telescope (VLT).
+					</div>
+					<ul>
+						<li>Developed tool kit for monitoring, analyzing and displaying relevant performance metrics for the Very Large Telescope (VLT).</li>
+						<li> Created models for parameters for re-calibration of the Very Large Telescope (VLT) positioning models</li>
+						<li> Created models for each focus of the Unit Telescope for the calibration and focus change processes.</li>
+						<li> Created models and analyzed quality parameters of images in pre-observation processes.</li>
+
+					</ul> 
+
+					<p>I worked The student worked in a team with Vitorrio Nurzia, the Systems and Optics chief, and astronomer Pascale Hibbon. The objective of this project was to create tools to be able to monitor the performance of the Very Large Telescope (VLT). Monitoring performance indicators of the telescope is a critical factor in achieving the goal of having the best possible instruments and technology for astronomic research. For this project, the objective was to focus on metrics most relevant for science operations.</p>
+					<p>The project was performed through Jupyter notebook files called dashboards, where each one had an implemented interface to make the information easily accessible for astronomers and Technical Instrument Operators (TIOs). The dashboards implemented by the students where UT Preset Sequence, UT Preset Time Analysis, Focus Change Sequence, Focus Change Time Analysis, Pointing Model Accuracy and Image Analysis Convergence Speed.</p>
+					<p>At the end of this project, all these dashboards were available for the professionals at ESO, transforming a large quantity of data into meaningful information. This project made it possible to develop company standards of focus change time, preset time and image quality, as well as tools in the Pointing Model Accuracy that will allow to determine when to update the mathematical model for better quality observations.</p>
+					<a href="Portafolio - ESO.pdf">
+
+					<div className="project-link">
+						<div className="project-link-icon">
+							<FontAwesomeIcon icon={faLink} />
+						</div>
+
+						<div className="project-link-text">Report pdf Link - Spanish </div>
+					</div>
+					</a>
+
+					<div className="project-image-container">
+					<	div className="project-image-wrapper">
+						<img
+							src="ESO.jpg"
+							alt="about"
+							className="project-image"
+						/>
+						</div>
+						
+					</div>
+					</div>
 					
 
 
